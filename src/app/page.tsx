34ae4e48fd0e9ce705f089e862dce4a46a60292a
Libo3 <valentinlibonati33@gmail.com>
2024-09-22 20:01:20 -0300

    Añadido feature compartir producto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ export default function Home() {
       <section className="px-8 pt-8 flex flex-col justify-center items-center gap-4">
         <ProductCard
           id={"1"}
-          imagen={
+          image={
             "https://res.cloudinary.com/dsuydyqgz/image/upload/v1715262308/07-catalogo-gri/ounthzvrsfniejttqadi.jpg"
           }
-          categoria={"Perfumeria"}
-          descripcion={"Perfume magico con olor"}
-          precio={8500}
+          category={"Perfumeria"}
+          description={"Perfume magico con olor"}
+          price={8500}
         />
         <ProductCard
           id={"2"}
-          imagen={
+          image={
             "https://res.cloudinary.com/dsuydyqgz/image/upload/v1694437727/01-varios/b4zoifzipxf6rz0czh4i.jpg"
           }
-          categoria={"Ropa interior"}
-          descripcion={"Ropa super extra suave"}
-          precio={9000}
+          category={"Ropa interior"}
+          description={"Ropa super extra suave"}
+          price={9000}
         />
       </section>
     </div>
--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,45 +1,86 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 interface ProductCardProps {
   id: string;
-  imagen: string;
-  categoria: string;
-  descripcion: string;
-  precio: number;
+  image: string;
+  category: string;
+  description: string;
+  price: number;
 }
 
 const ProductCard = ({
   id,
-  imagen,
-  categoria,
-  descripcion,
-  precio,
+  image,
+  category,
+  description,
+  price,
 }: Readonly<ProductCardProps>) => {
+  const [shared, setShared] = useState<boolean>(false);
+  const [shareError, setShareError] = useState<Error | undefined>();
+
+  const shareProduct = () => {
+    const product: ShareData = {
+      title: description,
+      text: "",
+      url: `http://192.168.0.190:3000/#${id}`,
+    };
+
+    if (navigator) {
+      navigator
+        .share(product)
+        .then(() => {
+          setShared(true);
+          setInterval(() => {
+            setShared(false);
+          }, 6000);
+        })
+        .catch((err: Error) =>
+          setShareError(new Error("No se pudo compartir"))
+        );
+    } else {
+      setShareError(new Error("Dispositivo incompatible"));
+    }
+  };
+
   return (
-    <article className="w-full bg-white rounded-lg">
+    <article id={id} className="w-full bg-white rounded-lg">
       <div className="w-full p-4 rounded-lg relative">
         <Image
           className="w-full h-64 object-cover object-center flex justify-center rounded-md"
-          src={imagen}
-          alt={descripcion}
+          src={image}
+          alt={description}
           width={1200}
           height={1200}
         />
         <div className="absolute bg-[#11111199] rounded-md bottom-6 right-6">
           <p className="text-white text-sm font-bold px-3 py-1">
-            $ {Intl.NumberFormat().format(precio)}
+            $ {Intl.NumberFormat().format(price)}
           </p>
         </div>
       </div>
       <div className="h-full px-4 flex flex-col justify-between gap-2">
-        <p className="text-[#2b5e2c] font-extrabold">{categoria}</p>
-        <p className="font-medium h-12 text-pretty">{descripcion}</p>
+        <p className="text-[#2b5e2c] font-extrabold">{category}</p>
+        <p className="font-medium h-12 text-pretty">{description}</p>
       </div>
       <div className="px-4 pt-4 pb-2">
-        <button className="uppercase border w-full py-2 rounded-xl font-medium">
-          compartir
-        </button>
+        {shareError === undefined ? (
+          <button
+            onClick={() => shareProduct()}
+            className={`uppercase duration-100 border w-full py-2 rounded-xl font-medium ${
+              shared ? " bg-[#79a456] text-white" : ""
+            }`}
+          >
+            COMPARTIR
+          </button>
+        ) : (
+          <p
+            className={`uppercase duration-100 border w-full py-2 text-sm bg-red-600 text-white text-center rounded-xl font-medium`}
+          >
+            {shareError.message}
+          </p>
+        )}
       </div>
     </article>
   );
