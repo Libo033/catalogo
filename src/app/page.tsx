488833f8e98c03da49fe1b93ab2d985b0ac3382c
Libo3 <valentinlibonati33@gmail.com>
2024-09-22 19:35:59 -0300

    Añadido parametros e interface para componente ProductCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
 import ProductCard from "@/components/cards/ProductCard";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <section className="px-8 pt-8 flex flex-col justify-center items-center gap-4">
-        <ProductCard />
-        <ProductCard />
+        <ProductCard
+          id={"1"}
+          imagen={
+            "https://res.cloudinary.com/dsuydyqgz/image/upload/v1715262308/07-catalogo-gri/ounthzvrsfniejttqadi.jpg"
+          }
+          categoria={"Perfumeria"}
+          descripcion={"Perfume magico con olor"}
+          precio={8500}
+        />
+        <ProductCard
+          id={"2"}
+          imagen={
+            "https://res.cloudinary.com/dsuydyqgz/image/upload/v1694437727/01-varios/b4zoifzipxf6rz0czh4i.jpg"
+          }
+          categoria={"Ropa interior"}
+          descripcion={"Ropa super extra suave"}
+          precio={9000}
+        />
       </section>
     </div>
   );
--- src/components/cards/ProductCard.tsx
+++ src/components/cards/ProductCard.tsx
@@ -1,25 +1,40 @@
 import Image from "next/image";
 import React from "react";
 
-const ProductCard = () => {
+interface ProductCardProps {
+  id: string;
+  imagen: string;
+  categoria: string;
+  descripcion: string;
+  precio: number;
+}
+
+const ProductCard = ({
+  id,
+  imagen,
+  categoria,
+  descripcion,
+  precio,
+}: Readonly<ProductCardProps>) => {
   return (
     <article className="w-full bg-white rounded-lg">
-      <div className="w-full p-4 rounded-lg">
+      <div className="w-full p-4 rounded-lg relative">
         <Image
-          className="w-full h-56 object-cover object-center flex justify-center rounded-md"
-          src={
-            "https://res.cloudinary.com/dsuydyqgz/image/upload/v1715262308/07-catalogo-gri/ounthzvrsfniejttqadi.jpg"
-          }
-          alt="producto"
+          className="w-full h-64 object-cover object-center flex justify-center rounded-md"
+          src={imagen}
+          alt={descripcion}
           width={1200}
           height={1200}
         />
+        <div className="absolute bg-[#11111199] rounded-md bottom-6 right-6">
+          <p className="text-white text-sm font-bold px-3 py-1">
+            $ {Intl.NumberFormat().format(precio)}
+          </p>
+        </div>
       </div>
       <div className="h-full px-4 flex flex-col justify-between gap-2">
-        <p className="text-[#2b5e2c] font-extrabold">Perfumeria</p>
-        <p className="font-medium h-12 text-pretty">
-          Perfume super duper ultra maximun olor epico las 32hs del dia
-        </p>
+        <p className="text-[#2b5e2c] font-extrabold">{categoria}</p>
+        <p className="font-medium h-12 text-pretty">{descripcion}</p>
       </div>
       <div className="px-4 pt-4 pb-2">
         <button className="uppercase border w-full py-2 rounded-xl font-medium">
