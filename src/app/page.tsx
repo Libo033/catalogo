28599ee71e9e44284629984d2e4f9d39c560b52b
Libo3 <valentinlibonati33@gmail.com>
2024-09-23 15:28:53 -0300

    Eliminado componente SearchBar de page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import ProductCard from "@/components/cards/ProductCard";
-import SearchBar from "@/components/other/SearchBar";
 
 export default function Home() {
   return (
     <div className="px-8 md:px-6">
-      <div className="flex gap-3 pt-6 sm:justify-between">
-        <SearchBar />
-      </div>
       <section className="py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-3">
         <ProductCard
           id={"1"}
