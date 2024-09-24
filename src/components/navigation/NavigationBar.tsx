0065bed4c053df39086ab70cc35a123422010bc4
Libo3 <valentinlibonati33@gmail.com>
2024-09-24 17:34:33 -0300

    feat: creado e implementado componente NavigationDrawer

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,14 +1,30 @@
+"use client";
 import { MenuRounded } from "@mui/icons-material";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import SearchBar from "../other/SearchBar";
+import NavigationDrawer from "./NavigationDrawer";
+import { Drawer } from "@mui/material";
 
 const NavigationBar = () => {
+  const [toggleDrawer, setToggleDrawer] = useState<boolean>(false);
+
+  const onOpenDrawer = useCallback(() => {
+    setToggleDrawer(true);
+  }, []);
+
+  const onCloseDrawer = useCallback(() => {
+    setToggleDrawer(false);
+  }, []);
+
   return (
     <nav className="bg-[#79a456] fixed grid w-full h-fit z-50 border-b border-[#2b5e2c] shadow-2xl">
       <ul className="flex justify-between items-center">
         <li className="py-5">
-          <Link className="text-3xl text-white font-bold px-8 sm:px-10 md:px-14" href={"/"}>
+          <Link
+            className="text-3xl text-white font-bold px-8 sm:px-10 md:px-14"
+            href={"/"}
+          >
             Catalogo
           </Link>
         </li>
@@ -16,12 +32,18 @@ const NavigationBar = () => {
           <SearchBar />
         </li>
         <li className="mx-8">
-          <MenuRounded sx={{ fontSize: "45px", color: "white" }} />
+          <MenuRounded
+            onClick={onOpenDrawer}
+            sx={{ fontSize: "45px", color: "white", cursor: "pointer" }}
+          />
         </li>
       </ul>
       <div className="mx-8 pt-2 pb-3 sm:hidden ">
         <SearchBar />
       </div>
+      <Drawer anchor="top" open={toggleDrawer} onClose={onCloseDrawer}>
+        <NavigationDrawer />
+      </Drawer>
     </nav>
   );
 };
---
+++ src/components/navigation/NavigationDrawer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavigationDrawer = () => {
+  return (
+    <div>NavigationDrawerNavigationDrawerNavigationDrawer</div>
+  )
+}
+
+export default NavigationDrawer
