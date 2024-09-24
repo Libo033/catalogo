ab65c53952ea8c162a04bdcafcf386c52a70fba2
Libo3 <valentinlibonati33@gmail.com>
2024-09-24 15:55:13 -0300

    Refactor: componente NavigationBar añadida nueva SearchBar

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -5,18 +5,21 @@ import SearchBar from "../other/SearchBar";
 
 const NavigationBar = () => {
   return (
-    <nav className="bg-[#79a456] fixed grid w-full h-fit pb-3 z-50 border-b border-[#2b5e2c] shadow-2xl">
+    <nav className="bg-[#79a456] fixed grid w-full h-fit z-50 border-b border-[#2b5e2c] shadow-2xl">
       <ul className="flex justify-between items-center">
-        <li className="py-4">
-          <Link className="text-3xl text-white font-bold pl-8" href={"/"}>
+        <li className="py-5">
+          <Link className="text-3xl text-white font-bold px-8 sm:px-10 md:px-14" href={"/"}>
             Catalogo
           </Link>
+        </li>
+        <li className="hidden sm:block sm:w-full">
+          <SearchBar />
         </li>
         <li className="mx-8">
           <MenuRounded sx={{ fontSize: "45px", color: "white" }} />
         </li>
       </ul>
-      <div className="mx-8 pt-2">
+      <div className="mx-8 pt-2 pb-3 sm:hidden ">
         <SearchBar />
       </div>
     </nav>
