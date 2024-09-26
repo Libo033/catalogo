40e4bf06ddfaaa7332c900340c332c21829ee60f
Libo3 <valentinlibonati33@gmail.com>
2024-09-26 15:02:01 -0300

    Fix de longitud de componente NavigationBar

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { MenuRounded } from "@mui/icons-material";
+import { FilterAlt, MenuRounded } from "@mui/icons-material";
 import Link from "next/link";
 import React, { useCallback, useState } from "react";
 import SearchBar from "../other/SearchBar";
@@ -18,42 +18,49 @@ const NavigationBar = () => {
   }, []);
 
   return (
-    <nav className="bg-[#79a456] fixed grid w-full h-fit z-50 border-b border-[#2b5e2c] shadow-2xl">
-      <ul className="flex justify-between items-center">
-        <li className="py-5">
-          <Link
-            className="text-3xl text-white font-bold px-8 sm:px-10 md:px-14"
-            href={"/"}
-          >
-            Catalogo
-          </Link>
-        </li>
-        <li className="hidden sm:block sm:w-full">
+    <div className="bg-[#79a456] fixed w-full z-50 shadow-xl">
+      <nav className="mx-auto grid max-w-screen-2xl w-full h-fit border-b border-[#2b5e2c]">
+        <ul className="flex justify-between items-center">
+          <li className="py-5">
+            <Link
+              className="text-3xl text-white font-bold px-8 sm:px-10 md:px-14"
+              href={"/"}
+            >
+              Catalogo
+            </Link>
+          </li>
+          <li className="hidden sm:block sm:w-full">
+            <SearchBar />
+          </li>
+          <li className="mx-8 md:hidden">
+            <MenuRounded
+              onClick={onOpenDrawer}
+              sx={{ fontSize: "45px", color: "white", cursor: "pointer" }}
+            />
+          </li>
+          <li className="hidden mx-8 md:block">
+            <FilterAlt
+              sx={{ fontSize: "45px", color: "white", cursor: "pointer" }}
+            />
+          </li>
+        </ul>
+        <div className="mx-8 pt-2 pb-3 sm:hidden ">
           <SearchBar />
-        </li>
-        <li className="mx-8">
-          <MenuRounded
-            onClick={onOpenDrawer}
-            sx={{ fontSize: "45px", color: "white", cursor: "pointer" }}
+        </div>
+        <Drawer anchor="top" open={toggleDrawer} onClose={onCloseDrawer}>
+          <NavigationDrawer
+            onCloseDrawer={onCloseDrawer}
+            category={[
+              "Perfumeria",
+              "Ropa Interior",
+              "Cremas",
+              "Ofertas",
+              "Cabello",
+            ]}
           />
-        </li>
-      </ul>
-      <div className="mx-8 pt-2 pb-3 sm:hidden ">
-        <SearchBar />
-      </div>
-      <Drawer anchor="top" open={toggleDrawer} onClose={onCloseDrawer}>
-        <NavigationDrawer
-          onCloseDrawer={onCloseDrawer}
-          category={[
-            "Perfumeria",
-            "Ropa Interior",
-            "Cremas",
-            "Ofertas",
-            "Cabello",
-          ]}
-        />
-      </Drawer>
-    </nav>
+        </Drawer>
+      </nav>
+    </div>
   );
 };
 
