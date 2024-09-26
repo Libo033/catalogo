4f84667d0c006d87d227afb2e6a0d8ea3f3df869
Libo3 <valentinlibonati33@gmail.com>
2024-09-26 11:05:16 -0300

    feat añadido filtro de precios en componente NavigationDrawer

--- src/components/navigation/NavigationDrawer.tsx
+++ src/components/navigation/NavigationDrawer.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Close } from "@mui/icons-material";
+import { Button, Slider, TextField } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 interface NavigationDrawerProps {
@@ -6,24 +9,64 @@ interface NavigationDrawerProps {
   category: Array<string>;
 }
 
+function valuetext(value: number) {
+  return `$ ${value}`;
+}
+
 const NavigationDrawer = ({
   onCloseDrawer,
   category,
 }: Readonly<NavigationDrawerProps>) => {
+  const [value1, setValue1] = React.useState<number[]>([20, 37]);
+
+  const handleChange1 = (
+    event: Event,
+    newValue: number | number[],
+    activeThumb: number
+  ) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - 0), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + 0)]);
+    }
+  };
+
   return (
-    <div className="bg-[#79a45660] overflow-hidden py-10 px-4">
+    <div className="overflow-hidden py-10 px-4">
       <div className="flex flex-col">
         <p className="text-2xl font-bold underline">Categorias</p>
         <div className="pt-2 flex flex-wrap gap-3">
           {category.map((cat) => (
-            <p
+            <Link
+              href={`/${cat}`}
               className="text-lg text-white font-medium bg-[#6a9ea999] w-fit px-5 border border-[#6a9ea9] rounded-lg"
               key={cat}
             >
               {cat}
-            </p>
+            </Link>
           ))}
         </div>
+      </div>
+      <div className="pt-8 flex flex-col">
+        <p className="text-2xl font-bold underline">Precio</p>
+        <div className="pt-2 pb-3 flex gap-3">
+          <TextField variant="outlined" label="$ Minimo" />
+          <TextField variant="outlined" label="$ Maximo" />
+        </div>
+        <Button
+          sx={{
+            color: "#fff",
+            borderColor: "#2b5e2c",
+            backgroundColor: "#2b5e2c",
+          }}
+          variant="contained"
+        >
+          Filtrar
+        </Button>
       </div>
       <div className="pt-10 flex justify-center">
         <Close
@@ -41,3 +84,5 @@ const NavigationDrawer = ({
 };
 
 export default NavigationDrawer;
+
+/* bg-[#79a45660] */
