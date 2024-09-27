146e7ca73510ac992ec4e6f244aa70f3ea86381c
Libo3 <valentinlibonati33@gmail.com>
2024-09-27 12:01:41 -0300

    feat añadido funcionamiento a componente SearchBar

--- src/components/other/SearchBar.tsx
+++ src/components/other/SearchBar.tsx
@@ -1,12 +1,30 @@
+"use client";
 import { Search } from "@mui/icons-material";
 import { InputAdornment, TextField } from "@mui/material";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const SearchBar = () => {
+  const [search, setSearch] = useState<string>("");
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (search === "") {
+      router.push(`${pathname}`);
+    } else {
+      router.push(`${pathname}?search=${search}`);
+    }
+
+    return () => {};
+  }, [search]);
+
   return (
     <div className="bg-transparent w-full sm:max-w-96">
       <TextField
         fullWidth
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
         autoComplete="off"
         placeholder="Buscar producto"
         InputProps={{
