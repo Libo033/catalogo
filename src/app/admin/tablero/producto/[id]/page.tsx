ae59fba8972a2b6e7be8aa7c727a3323e3c9c07e
Libo3 <valentinlibonati33@gmail.com>
2024-10-01 14:32:25 -0300

    feat: agregado boton de volver a /producto y /producto/[id]

--- src/app/admin/tablero/producto/[id]/page.tsx
+++ src/app/admin/tablero/producto/[id]/page.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const page = ({ params }: { params: { id: string } }) => {
   return (
     <div className="mx-auto max-w-screen-2xl">
       <div className="pt-[137px] px-8 sm:pt-[76px] md:px-6">
-        Editar Producto: {params.id}
+        <div className="pt-4">
+          <Link
+            className="flex items-center gap-2 underline text-blue-600"
+            href={"/admin/tablero"}
+          >
+            Volver
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/app/admin/tablero/producto/page.tsx
+++ src/app/admin/tablero/producto/page.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div className="mx-auto max-w-screen-2xl">
-      <div className="pt-[137px] px-8 sm:pt-[76px] md:px-6">Nuevo Producto</div>
+      <div className="pt-[137px] px-8 sm:pt-[76px] md:px-6">
+        <div className="pt-4">
+          <Link
+            className="flex items-center gap-2 underline text-blue-600"
+            href={"/admin/tablero"}
+          >
+            Volver
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
