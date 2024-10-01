b9aa1ccc579d66a73b24e01582a92ede0d22edf3
Libo3 <valentinlibonati33@gmail.com>
2024-10-01 10:35:40 -0300

    feat: creado componente ProductCardAdmin

--- src/app/admin/tablero/page.tsx
+++ src/app/admin/tablero/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import ProductCardAdmin from "@/components/cards/ProductCardAdmin";
+import { ProductCardProps } from "@/lib/interfaces";
+import React from "react";
+
+const prods: Array<ProductCardProps> = [
+  {
+    id: "1",
+    image:
+      "https://res.cloudinary.com/dsuydyqgz/image/upload/v1694437727/01-varios/b4zoifzipxf6rz0czh4i.jpg",
+    category: "Perfumeria",
+    description: "Perfume 123",
+    price: 10000,
+    sale: false,
+    various: ["Amaderado"],
+  },
+  {
+    id: "2",
+    image:
+      "https://res.cloudinary.com/dsuydyqgz/image/upload/v1715262308/07-catalogo-gri/ounthzvrsfniejttqadi.jpg",
+    category: "Productos",
+    description: "Crema de planta",
+    price: 10000,
+    sale: false,
+    various: [],
+  },
+  {
+    id: "3",
+    image:
+      "https://res.cloudinary.com/dsuydyqgz/image/upload/v1694437727/01-varios/b4zoifzipxf6rz0czh4i.jpg",
+    category: "Ropa Interior",
+    description: "Ropa 123456",
+    price: 9999,
+    sale: true,
+    various: ["Talle M"],
+  },
+  {
+    id: "4",
+    image:
+      "https://res.cloudinary.com/dsuydyqgz/image/upload/v1715262308/07-catalogo-gri/ounthzvrsfniejttqadi.jpg",
+    category: "Perfumeria",
+    description: "Perfume 999",
+    price: 119999,
+    sale: true,
+    various: ["Citrico"],
+  },
+];
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="mx-auto max-w-screen-2xl">
+      <div className="pt-[137px] px-8 sm:pt-[76px] md:px-6">
+        <section className="py-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-3">
+          {prods.map((p) => (
+            <ProductCardAdmin
+              key={p.id + p.image}
+              id={p.id}
+              image={p.image}
+              category={p.category}
+              description={p.description}
+              price={p.price}
+              sale={p.sale}
+              various={p.various}
+            />
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
---
+++ src/components/cards/ProductCardAdmin.tsx
@@ -0,0 +1,66 @@
+import { ProductCardProps } from "@/lib/interfaces";
+import { Button, Chip } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+const ProductCardAdmin = ({
+  id,
+  image,
+  category,
+  description,
+  price,
+  sale,
+  various,
+}: Readonly<ProductCardProps>) => {
+  return (
+    <article
+      id={id}
+      className={`w-full flex flex-col bg-white border shadow-lg rounded-lg`}
+    >
+      <div className="w-full p-4 rounded-lg relative">
+        <Image
+          className="w-full h-64 object-cover object-center flex justify-center rounded-md"
+          src={image}
+          alt={description}
+          width={1200}
+          height={1200}
+        />
+        {sale ? (
+          <div className="absolute bg-[#f9f971] rounded-md bottom-6 right-6">
+            <p className="text-black text-sm font-bold px-3 py-1">
+              OFERTA $ {price}
+            </p>
+          </div>
+        ) : (
+          <div className="absolute bg-[#11111199] rounded-md bottom-6 right-6">
+            <p className="text-white text-sm font-bold px-3 py-1">$ {price}</p>
+          </div>
+        )}
+      </div>
+      <div className="h-full px-4 flex flex-col justify-between gap-2">
+        <p className="text-[#2b5e2c] font-extrabold">{category}</p>
+        <p className="font-medium h-12 text-pretty">{description}</p>
+      </div>
+      <div className="min-h-[28px] mx-4 grid grid-flow-col gap-2 overflow-x-auto overflow-y-hidden">
+        {various.map((ch, i) => (
+          <Chip
+            key={i + ch}
+            label={ch}
+            size="small"
+            sx={{ backgroundColor: "#c5d86e", width: "fit-content" }}
+          />
+        ))}
+      </div>
+      <div className="px-4 pt-4 pb-4 flex gap-2">
+        <Button fullWidth color="primary" variant="contained">
+          editar
+        </Button>
+        <Button fullWidth color="error" variant="contained">
+          borrar
+        </Button>
+      </div>
+    </article>
+  );
+};
+
+export default ProductCardAdmin;
