54ef61f43da8bdc5c76ed12af85850c934275f81
Libo3 <valentinlibonati33@gmail.com>
2024-10-02 16:58:56 -0300

    Feat: API/DELETE method para eliminar imagen de Cld

--- src/lib/cloudinary/handleDeleteImage.tsx
+++ src/lib/cloudinary/handleDeleteImage.tsx
@@ -1,13 +1,13 @@
-export const handleDeleteImageFromCld = (
+export const handleDeleteImageFromCld = async (
   img: string,
   upload_preset: string
-): boolean | Error => {
+): Promise<boolean | Error> => {
   const public_id: string = img.slice(
     img.indexOf(upload_preset) + upload_preset.length,
     img.length - 4
   );
 
-  fetch(`/api/v1/image/${public_id}`, { method: "DELETE" })
+  await fetch(`/api/v1/cld${public_id}`, { method: "DELETE" })
     .then((res) => res.json())
     .then((data) => {
       if (data.code === 200) {
